387687438826c26cd3dd869fd21c68a00a148f38
Marc Bakker <webdev@marcbakker.com>
2021-03-08 19:41:38 +0100

    Signup form: setup and add email input field.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
-
+import { FormControl, Validators } from "@angular/forms";
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'signup-form';
+	title = 'signup-form';
+	email = new FormControl("", [Validators.required, Validators.email]);
+
+	getErrorMessage() {
+		console.log(this.email.hasError('email'))
+		return this.email.hasError('email')
+			? 'Not a valid email'
+			: this.email.hasError('required')
+				? "Please enter a valid email address"
+				: "";
+	}
+
+	validateEmail = (emailAddress: FormControl) => {
+		return { invalidEmail: "email address invalid" }
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-
+import { FormInputComponent } from './components/form-input/form-input.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		FormInputComponent,
+	],
+	imports: [
+		BrowserModule,
+		MatInputModule,
+		MatFormFieldModule,
+		MatInputModule,
+		NoopAnimationsModule,
+		FormsModule,
+		ReactiveFormsModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
