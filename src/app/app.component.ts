f40e01c051f642272798a20a552e2dc96db260f7
Marc Bakker <webdev@marcbakker.com>
2021-03-08 21:59:48 +0100

    Add fields for firstName, lastName, email and password + validation.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,18 +7,38 @@ import { FormControl, Validators } from "@angular/forms";
 })
 export class AppComponent {
 	title = 'signup-form';
-	email = new FormControl("", [Validators.required, Validators.email]);
+	firstName = new FormControl("", [Validators.required]);
+	lastName = new FormControl("", [Validators.required]);
+	email = new FormControl("", [Validators.required]);
+	password = new FormControl("", [Validators.required]);
 
-	getErrorMessage() {
-		console.log(this.email.hasError('email'))
-		return this.email.hasError('email')
+	getFirstNameErrorMessage() {
+		console.log(this.firstName)
+		return this.firstName.touched && this.firstName.hasError('required')
+			? "Please enter your first name"
+			: "";
+	}
+
+	getLastNameErrorMessage() {
+		console.log(this.lastName)
+		return this.lastName.touched && this.lastName.hasError('required')
+			? "Please enter your surname"
+			: "";
+	}
+
+	getEmailErrorMessage() {
+		console.log(this.email)
+		return this.email.touched && this.email.hasError('email')
 			? 'Not a valid email'
-			: this.email.hasError('required')
+			: this.email.touched && this.email.hasError('required')
 				? "Please enter a valid email address"
 				: "";
 	}
 
-	validateEmail = (emailAddress: FormControl) => {
-		return { invalidEmail: "email address invalid" }
+	getPasswordErrorMessage() {
+		console.log(this.password)
+		return this.password.touched && this.password.hasError('required')
+			? "Please enter your password"
+			: "";
 	}
 }
