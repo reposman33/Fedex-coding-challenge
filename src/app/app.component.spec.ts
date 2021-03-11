004c8fd1bded1904ecd49f819839fefba8a54b2e
Marc Bakker <webdev@marcbakker.com>
2021-03-12 00:25:12 +0100

    Added some tests.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -11,16 +11,9 @@ describe('AppComponent', () => {
 		}).compileComponents();
 	});
 
-	it('should create the app', () => {
+	it('should render', () => {
 		const fixture = TestBed.createComponent(AppComponent);
 		const app = fixture.componentInstance;
-		expect(app).toBeTruthy();
+		expect(app).toBeDefined();
 	});
-
-	it("should display error message when no email address provided", () => {
-		const fixture = TestBed.createComponent(AppComponent);
-		const compiled = fixture.nativeElement;
-		// expect(compiled.querySelector('.content span').textContent).toContain('signup-form app is running!');
-		console.log("compiled = ", compiled)
-	})
 });
--- src/app/components/sign-up-form/sign-up-form.component.spec.ts
+++ src/app/components/sign-up-form/sign-up-form.component.spec.ts
@@ -1,25 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SignUpFormComponent } from './sign-up-form.component';
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 
 describe('SignUpFormComponent', () => {
-  let component: SignUpFormComponent;
-  let fixture: ComponentFixture<SignUpFormComponent>;
+	let component: SignUpFormComponent;
+	let fixture: ComponentFixture<SignUpFormComponent>;
+	let element: HTMLElement;
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			declarations: [component],
+			imports: [HttpClientTestingModule]
+		})
+			.compileComponents();
+	});
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ SignUpFormComponent ]
-    })
-    .compileComponents();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(SignUpFormComponent);
+		component = fixture.componentInstance;
+		element = fixture.nativeElement;
+		spyOn(component, "onSubmitSigninForm");
+	});
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SignUpFormComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it("should POST the sign up form", () => {
+		const signUpForm = {
+			value: {
+				firstName: "Marc",
+				lastName: "Bakker",
+				email: "marc@bakker.com"
+			}
+		}
+		expect(component).toBeDefined();
+		component.onSubmitSigninForm()
+		expect(component.onSubmitSigninForm).toHaveBeenCalled();
+	})
 });
